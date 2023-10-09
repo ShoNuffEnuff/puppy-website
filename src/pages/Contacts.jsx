46882f0992e29cc3782e7d2da5c653bc3764bf7d
SharnaJV <134357065+SharnaJV@users.noreply.github.com>
2023-10-09 15:08:25 +1030

    Contacts
    I have added necessary features to our contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import ContactDetails from '../components/ContactDetails';
+import GoogleMap from '../components/GoogleMap';
+import './Contacts.css';
 const Contacts = () => {
     return (
-        <div className="Contacts">
-            <h4>Contacts</h4>
-            
+        <div className= 'contactData'>
+            <div>
+                <ContactDetails className= 'CInfoTable'/> 
+            </div>
+            <div>
+                <GoogleMap className= 'mapLocation'/>
+            </div>
+
         </div>
-    );
+    )    
+
 }
 
 export default Contacts;
